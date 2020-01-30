d6c7b54cf5045b986660041038f8397d40e478d1
Hozen Li <Hozen@live.com>
2020-01-30 18:23:20 +0800

    arrange from Lei Li

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -47,5 +48,6 @@ module.exports = {
       filename: "[name][hash].css",
       chunkFilename: "[id][hash].css",
     }),
+    new CleanWebpackPlugin(),
   ],
 }
