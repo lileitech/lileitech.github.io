39e03ce7a38e0d102ce57098320288146fdd8430
Hozen Li <Hozen@live.com>
2020-01-30 18:08:45 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import './style/index.css';
+
+function main() {
+  function $(selector) {
+    return document.querySelector(selector);
+  }
+
+  var extractFaceEL = $('#extractFace');
+  var extractBodyEL = $('#extractBody')
+  var maskEL = $('#mask')
+  var imageViewerEL = $('#imageViewer')
+  var imageViewerImgEL = $('#imageViewerImg')
+  var mainEL = $('#main')
+  var mainOuterEL = $('#mainOuter')
+  var extractInfoEL = $('#extractInfo')
+  var isShowDialog = false
+  var pageNum = 0
+  var detailEL = $('#detail')
+  var contentEL = $('#content')
+  var moreBtnOuterEL = $('#moreBtnOuter')
+
+  function showDetail() {
+    if (pageNum != 0 || isShowDialog) return
+    mainEL.className = 'main main--blur'
+    extractInfoEL.className = 'extract-info extract-info--small'
+    extractFaceEL.className = 'extract_face extract_face--small'
+    extractBodyEL.className = 'extract_body extract_body--more'
+    moreBtnOuterEL.className = "more-btn-outer more-btn-outer--hidden"
+    contentEL.className = 'content content--more'
+    detailEL.className = 'detail detail--show'
+    pageNum = !pageNum
+  }
+
+  function clearDialog() {
+    imageViewerEL.style.display = 'none'
+    maskEL.style.display = 'none'
+    isShowDialog = false
+  }
+
+  function maskTrigger(isShow, params) {
+    maskEL.style.display = isShow ? 'block' : 'none'
+  }
+  function popImagViwer(img, params) {
+    // TODO img 可处理对象字符串，可加参数params
+    maskTrigger(true);
+    isShowDialog = true;
+    imageViewerEL.style.display = 'block';
+    imageViewerImgEL.src = img;
+  }
+  function handleFaceClick(event) {
+    event.stopPropagation();
+    img = window.getComputedStyle(event.target, null).backgroundImage;
+    img = img.replace(/([u,U][r,R][l,L]\("?)([^"]*)"?\)/, '$2')
+    popImagViwer(img)
+  }
+
+  maskEL.addEventListener('click', clearDialog)
+  document.addEventListener('keydown', showDetail)
+  mainOuterEL.addEventListener('click', showDetail)
+  extractFaceEL.addEventListener('click', handleFaceClick)
+  extractFaceEL.addEventListener('touch', clearDialog)
+
+}
+document.addEventListener('DOMContentLoaded', main)
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/, use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+            },
+          },
+          'postcss-loader', 'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          }
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name][hash].css",
+      chunkFilename: "[id][hash].css",
+    }),
+  ],
+}
