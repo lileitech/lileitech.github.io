c4e63af2e22b2d60cbe3f9305a078cb6f491afb2
Hozen Li <Hozen@live.com>
2020-06-07 01:20:16 +0800

    论文列表

---
+++ src/pages/publications/index.js
@@ -0,0 +1,7 @@
+import './index.css'
+
+function main() {
+
+}
+
+document.addEventListener('DOMContentLoaded', main)
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    publications: './src/pages/publications'
+  },
+  devServer: {
+    port: 9409,
+    publicPath: '/',
+    contentBase: path.join(__dirname, 'public')
+  },
+  resolve: {
+    alias: {
+      assets: path.join(__dirname, './src/style/assets')
+    },
+  },
   module: {
     rules: [
       {
@@ -35,6 +50,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      filename: 'index.html',
+      chunks: ['index'],
       minify: {
         collapseWhitespace: true,
         removeComments: true,
@@ -44,10 +61,30 @@ module.exports = {
         useShortDoctype: true
       },
     }),
+
+    new HtmlWebpackPlugin({
+      template: './src/pages/publications/index.html',
+      filename: 'publications/index.html',
+      chunks: ['publications'],
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      },
+    }),
+
     new MiniCssExtractPlugin({
       filename: "[name][hash].css",
       chunkFilename: "[id][hash].css",
     }),
+
     new CleanWebpackPlugin(),
+
+    new CopyWebpackPlugin({
+      patterns: [{ from: './src/public', to: 'public' }]
+    })
   ],
 }
