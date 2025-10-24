ee30137ae21cbac543536f5c197d2ffc23135661
Lei Li <lei.li@nus.edu.sg>
2025-10-24 13:44:31 +0800

    Site updated: 2025-10-24 13:44:29

---
+++ js/page.js
@@ -0,0 +1,96 @@
+/* tool fun */
+
+function $(s) {
+  return document.querySelector(s);
+}
+
+/* page */
+
+window.onload = main();
+
+function main() {
+
+  var tocEl = $('#outline');
+  var topMargin = 80;
+  var tocElInaitialAbsTop = tocEl.style.top;
+  var tocElInaitialTop = getTopDistance2Doc(tocEl);
+
+
+  var menuEl = $('#menu');
+  var menuToggleEl = $('#menuToggle');
+
+
+  var menuToggle = {
+    isExpand: false,
+
+    sleep() {
+      menuEl.style.height = "auto";
+      this.isExpand = false;
+    },
+
+    expand() {
+      menuEl.style.height = window.innerHeight - 60 + 'px';
+      this.isExpand = true;
+    },
+
+    fold() {
+      menuEl.style.height = "0px";
+      this.isExpand = false;
+    },
+
+    toggle() {
+      console.log(this);
+      if (this.isExpand) this.fold();
+      else this.expand();
+    }
+  };
+
+
+  setTocPos();
+  document.addEventListener("scroll", setTocPos);
+
+
+  menuToggleEl.addEventListener('click', function (e) { menuToggle.toggle(); });
+  menuToggleEl.addEventListener('tap', function (e) { menuToggle.toggle(); });
+
+  window.addEventListener('resize', resizeHanlder());
+
+  function resizeHanlder() {
+    var formerWidth = window.innerWidth;
+    return function () {
+      widthBreakpointHanlder(formerWidth);
+      formerWidth = window.innerWidth;
+    };
+  }
+
+  function widthBreakpointHanlder(formerWidth) {
+
+    if (formerWidth < 767 && window.innerWidth >= 767) {
+      console.info('to desktop breakpoint');
+      menuToggle.sleep();
+    }
+
+    if (formerWidth >= 767 && window.innerWidth < 767) {
+      console.info('to mobile breakpoint');
+      menuToggle.fold();
+    }
+  }
+
+  function getTopDistance2Doc(node) {
+    var res = node.offsetTop || 0;
+    if (node.parentNode) return res + getTopDistance2Doc(node.parentNode);
+    return res;
+  }
+
+  function setTocPos() {
+    var htmlDOM = document.documentElement;
+    if (htmlDOM.scrollTop > tocElInaitialTop - topMargin) {
+      tocEl.style.position = "fixed";
+      tocEl.style.top = topMargin + "px";
+    } else {
+      tocEl.style.position = "absolute";
+      tocEl.style.top = tocElInaitialAbsTop;
+    }
+  }
+
+}
